fae9d8c3536f987a241bd317e0d8567411e7b812
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-11-04 12:34:15 +0600

    added goods

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mealImg from './assets/meal.png'
+import drinkImg from './assets/drink.png'
 import './App.css'
 
+interface goodsInterface {
+  name: string;
+  count: number;
+  img: string;
+  id: number;
+  price: number;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [goods, setFoods] = useState<goodsInterface[]>([
+    {img: mealImg,  name: "Hamburger", count: 0, id: 1, price: 80},
+    {img: mealImg,  name: "Cheeseburger", count: 0, id: 2, price: 90},
+    {img: mealImg,  name: "Fries", count: 0, id: 3, price: 45},
+    {img: drinkImg,  name: "Coffee", count: 0, id: 4, price: 70},
+    {img: drinkImg,  name: "Tea", count: 0, id: 5, price: 50},
+    {img: drinkImg,  name: "Cola", count: 0, id: 6, price: 40},
+  ]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="goodsList">
+          {goods.map(item => (
+            <div className="goods">
+              <div></div>
+              <img  src={item.img} className="goodsImg" alt={item.name}/>
+              <p>{item.name}</p>
+              <p className="priceString">{item.price}</p></div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
